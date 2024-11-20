2f704bd1b373765eeea9f24196700078ec735ef3
sakshiS6 <sakshimishra2004545@gmail.com>
2024-11-20 19:11:03 +0530

    Connected tothe backend

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const UserRouter = require('./routers/userRouter');
 const ProductRouter = require('./routers/productRouter');
+const cors = require('cors')
 
 //creating an express app
 
@@ -10,6 +11,11 @@ const app = express();
 const port = 5000;
 
 //middleware
+
+app.use(cors({
+    origin:['http://localhost:3000'],
+}))
+
 app.use(express.json())
 app.use('/user', UserRouter);   
 app.use('/product', ProductRouter);   
